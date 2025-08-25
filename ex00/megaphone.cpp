8304d9ad9c958c486d56087bb44b21138162b2c0
Dmitrijs Lasko <dmitrijs.lasko@gmail.com>
2025-08-25 16:07:38 +0200

    first commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>   // for std::string
+#include <cctype>   // for std::toupper
+
+int main(int argc, char **argv) 
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		std::string s(argv[i]);
+		for (std::size_t j = 0; j < s.size(); ++j) {
+			s[j] = static_cast<char>(std::toupper(static_cast<unsigned char>(s[j])));
+		}
+		std::cout << s;
+	}
+	std::cout << '\n';
+	return 0;
+}
