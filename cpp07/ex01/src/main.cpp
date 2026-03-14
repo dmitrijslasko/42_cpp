91214943a0ef9b5cc15b1492be82130feaa56e6b
Dmitrijs Lasko (XCode) <dmitrijs.lasko@gmail.com>
2026-03-14 22:51:30 +0100

    ex01 added

---
+++ cpp07/ex01/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "iter.hpp"
+#include "utils.hpp"
+
+template <typename T>
+void print(const T &x)
+{
+    std::cout << x << " ";
+}
+
+void addOne(int &x) {
+	x += 1;
+}
+
+void hideValue(const int &x) {
+	(void) x;
+	std::cout << "* ";
+}
+
+int main() {
+	{
+		printSeparator();
+		int arr[] = {1, 2, 3, 4, 5};
+		std::cout << "Original array:" << std::endl;
+		iter(arr, 5, print);
+		std::cout << std::endl;
+		printSeparator();
+		iter(arr, 5, addOne);
+		std::cout << "Array after adding 1 to each element:" << std::endl;
+		iter(arr, 5, print);
+		std::cout << std::endl;
+		printSeparator();
+		iter(arr, 5, hideValue);
+		std::cout << std::endl;
+		printSeparator();
+	}
+
+	return 0;
+}
